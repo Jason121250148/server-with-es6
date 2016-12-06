f4ac8b9af2b3cdfa20abda365b87ad0f61c99ba7
Jason121250148 <962215093@qq.com>
2016-12-06 11:08:20 +0800

    - Integrated.

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+import config from "config";//不能加./，不然就是找config下的index
+
 import app from "./lib/app";
 import httpServer from "./lib/http/server";
 
-const PORT = 8000;
-app.attach(httpServer);
-httpServer.listen(PORT, () => {
-    console.log(`The server is now listening at port ${PORT}...`);
-});
+
+(function main()
+{
+    checkEnvironment();
+    startHttpServer();
+})();
+
+
+function checkEnvironment()
+{
+    const env = app.get("env");
+    if (env === "development")
+    {
+        console.log("Server is now running at [DEVELOPMENT] mode.");
+    }
+    else if (env === "production")
+    {
+        console.log("Server is now running at [PRODUCTON] mode.");
+    }
+    else
+    {
+        throw new Error("Unsupported mode.");
+    }
+}
+
+function startHttpServer()
+{
+    const port = parseInt(config.get("http.port"));
+    if (isNaN(port))
+    {
+        throw new Error(`"http.port" must be number.`);
+    }
+    app.attach(httpServer);
+    httpServer.listen(port, () => {
+        console.log(`The server is now listening at port ${port}...`);
+    });
+}
